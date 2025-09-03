4318fc9a12e4ed9514bd12091c8dd81b77c06db7
HabilisErgastusCaesar <bartscheepmaker@hotmail.com>
2025-09-03 08:21:05 +0200

    Update mainLayout and other components

--- components/stargate/mainLayout.tsx
+++ components/stargate/mainLayout.tsx
@@ -1,4 +1,14 @@
+import { useStargateContext } from "./useContext/stargateComponents"
+
 export const MainLayout = ({router}) => {
-    console.log(router)
-    return(<h1>main layout</h1>)
+    console.log(router);
+    const { Context } = useStargateContext();
+    console.log(Context.windowWidth.optionButtons)
+    return(<div>
+        {Context.windowWidth.optionButtons ? (
+            <h1>small screen</h1>
+        ):(
+            <h1>big screen</h1>
+        )}
+        </div>)
 }
--- pages/stargate/[stargatePage].tsx
+++ pages/stargate/[stargatePage].tsx
@@ -1,10 +1,65 @@
 import { useRouter } from "next/router"
+import { useEffect } from "react";
 
+import { useStargateContext } from "../../components/stargate/useContext/stargateComponents";
 import { MainLayout } from "../../components/stargate/mainLayout";
 import { UpperBar } from "../../components/stargate/upperBar";
 
 const StargatePage = () => {
     const router = useRouter();
+    const { Context } = useStargateContext();
+
+    const debounce = (func, delay) => {
+        let timeoutId;
+        return (...args) => {
+            if (timeoutId) {
+                clearTimeout(timeoutId);
+            }
+            timeoutId = setTimeout(() => {
+                func.apply(null, args);
+            }, delay);
+        };
+    };
+    
+    useEffect(() => {
+        const handleResize = debounce(() => { 
+            if (window.innerWidth <= 830) { 
+                if (!Context.windowWidth.searchFilter) {
+                    Context.setWindowWidth((prev) => ({
+                        ...prev,
+                        searchFilter:true
+                    }));
+                }
+            } else {
+                if (Context.windowWidth.searchFilter) {
+                    Context.setWindowWidth((prev) => ({
+                        ...prev,
+                        searchFilter:false
+                    }));
+                }
+            } if (window.innerWidth <= 1199) { 
+                if (!Context.windowWidth.optionButtons) {
+                    Context.setWindowWidth((prev) => ({
+                        ...prev,
+                        optionButtons:true
+                    }));
+                }
+            } else {
+                if (Context.windowWidth.optionButtons) {
+                    Context.setWindowWidth((prev) => ({
+                        ...prev,
+                        optionButtons:false
+                    }));
+                }
+            }
+        }, 300); 
+        if (typeof window !== "undefined") 
+            { handleResize(); window.addEventListener("resize", handleResize); } 
+        return () => { 
+            if (typeof window !== "undefined") 
+                { window.removeEventListener("resize", handleResize); 
+        } };
+    },[Context.windowWidth])
     
     return(<div>
         {router.query.stargatePage && <>
